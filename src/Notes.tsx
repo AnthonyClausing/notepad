beb6f9de8a55306304fca6c3177d5bb78aec4bca
AnthonyClausing <AnthonyClausing@gmail.com>
2020-12-28 19:57:35 -0500

    removed old notepad code moved to react component

--- src/Notes.tsx
+++ src/Notes.tsx
@@ -2,10 +2,30 @@
 import React from 'react';
 import './Notes.css';
 
-function Notes() {
-  return (
-    <textarea id="area" className="notes"></textarea>
-  );
+type props = {};
+type state = {text: string}
+class Notes extends React.Component<props,state> {
+  state: state = {
+    text: ""
+  }
+
+  saveNotes(evt: any){
+    if(evt){
+      evt.preventDefault()
+      chrome.storage.sync.set({"notes": evt && evt.target.value})
+    }
+  }
+
+  componentWillMount(){
+    chrome.storage.sync.get(["notes"], (result) => this.setState({text: result["notes"]}))
+  }
+
+  render(){
+    return (
+      <textarea className="notes" onChange={this.saveNotes} value={this.state.text}></textarea>
+    );
+  }
+  
 }
 
 export default Notes;
