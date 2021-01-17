ba7430f1affe77f394fac1922602cc6888ea94c3
AnthonyClausing <AnthonyClausing@gmail.com>
2021-01-16 22:29:22 -0500

    refactor notes + css changes

--- src/Notes.tsx
+++ src/Notes.tsx
@@ -2,30 +2,34 @@
 import React from 'react';
 import './Notes.css';
 
+//REFACTOR: Functional Component???
 type props = {};
 type state = {text: string}
 class Notes extends React.Component<props,state> {
   state: state = {
     text: ""
   }
-
-  saveNotes(evt: any){
-    if(evt){
-      evt.preventDefault()
-      chrome.storage.sync.set({"notes": evt && evt.target.value})
-    }
-  }
-
-  componentWillMount(){
+  getNotes() {
     chrome.storage.sync.get(["notes"], (result) => this.setState({text: result["notes"]}))
+  }
+  saveNotes(){
+    chrome.storage.sync.set({"notes": this.state.text})
+  }
+  handleInput(evt: any){
+    this.setState({text: evt.target.value})
+  }
+  componentWillMount(){
+    this.getNotes()
+  }
+  componentWillUnmount(){
+    this.saveNotes()
   }
 
   render(){
     return (
-      <textarea id="notes" onChange={this.saveNotes} value={this.state.text}></textarea>
+      <textarea id="notes" value={this.state.text} onInput={(evt) => this.handleInput(evt)} onChange={() => this.saveNotes()}></textarea>
     );
-  }
-  
+  } 
 }
 
 export default Notes;
